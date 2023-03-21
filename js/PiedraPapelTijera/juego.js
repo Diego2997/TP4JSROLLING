0220e7465b0dadc6936f657fb3ee13bd176b2991
Diego Mercado <diego.mercado1997@hotmail.com>
2023-03-21 08:49:18 -0300

    juego piedra papel tijera

---
+++ js/PiedraPapelTijera/juego.js
@@ -0,0 +1,39 @@
+const opciones = ["piedra", "papel", "tijera"];
+
+function obtenerMovimiento() {
+  const opcion = prompt(
+    "Elige tu movimiento (piedra, papel o tijera): "
+  ).toLowerCase();
+  if (opciones.includes(opcion)) {
+    return opcion;
+  } else {
+    document.write("Movimiento no válido");
+    return getUserChoice();
+  }
+}
+
+function MovimientoPC() {
+  const randomIndex = Math.floor(Math.random() * opciones.length);
+  return opciones[randomIndex];
+}
+
+let jugaOtraVez = "s";
+do {
+  const usuario = obtenerMovimiento();
+  const computadora = MovimientoPC();
+  document.write(`Tu movimiento: ${usuario}`);
+  document.write(` , Movimiento de la PC: ${computadora}`);
+  if (usuario === computadora) {
+    document.write("Empate!");
+  } else if (
+    (usuario === "piedra" && computadora === "tijera") ||
+    (usuario === "papel" && computadora === "piedra") ||
+    (usuario === "tijera" && computadora === "papel")
+  ) {
+    document.write(" Ganaste!");
+  } else {
+    document.write(" Perdiste!");
+  }
+  jugaOtraVez = prompt("Quieres jugar otra vez?").toLowerCase();
+} while (jugaOtraVez === "s");
+alert("Gracias por jugar");
